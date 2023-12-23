0c2486103913d267768fe9a6158d22a5528264a8
milou2764 <emilien.andre2764@gmail.com>
2023-12-23 19:34:31 +0100

    clean api

--- scripts/api.py
+++ scripts/api.py
@@ -3,23 +3,16 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch('http://localhost:9200')
 index='iscx'
 
-def get_fields(key: str) -> list:
+###########
+# GENERAL #
+###########
 
-
-def get_protocols() -> list:
-    """
-    Returns the list of all the (distinct) protocols contained in the XML files
-    """
-    field = 'protocolName'
-    protocols = []
-    aggregation_query = {'unique_values':{'terms':{'field':field}}}
-    result = es.search(index=index, aggs=aggregation_query)
-    protocols += [bucket["key"] for bucket in result["aggregations"]["unique_values"]["buckets"]]
-    return protocols
-
-def get_term_flows(key: str, value: str) -> list:
+def get_documents(field: str, value: str) -> list:
+    '''
+    Returns all the documents having the specific field and value couple
+    '''
     flows=[]
-    query = {'term':{key:value}} 
+    query = {'term':{field:value}} 
     initial_results=es.search(index=index,query=query,scroll='1m')
     scroll_id=initial_results['_scroll_id'] 
     while True:
@@ -33,185 +26,129 @@ def get_term_flows(key: str, value: str) -> list:
         flows+=[e['_source'] for e in next_results]
     return flows
 
+def get_values(field: str) -> list:
+    '''
+    Returns all the distinct values in a specific field in all documents
+    '''
+    values = []
+    aggregation_query = {'unique_values':{'terms':{'field':field,'size':1000000}}}
+    result = es.search(index=index, aggs=aggregation_query)
+    values += [bucket["key"] for bucket in result["aggregations"]["unique_values"]["buckets"]]
+    return values
+
+def get_documents_count(field: str, value: str) -> int:
+    '''
+    Returns the number of documents having the specific field and value couple
+    '''
+    search_query = {'match': {field: value}}
+    return es.count(index=index, query=search_query)['count']
+
+def get_totals(field: str, srcField: str, dstField: str) -> dict:
+    '''
+    Returns the totals of srcField and dstField sorted with the values of field
+    '''
+    values = get_values(field)
+    totals = {}
+    for value in values:
+        totals[value] = {'source': 0, 'destination': 0}
+        query = {
+                'term': {
+                    field: value
+                    }
+                }
+        aggregation_query = {
+                'total_sum_source': {
+                    'sum': {
+                        'field': srcField
+                        }
+                    },
+                'total_sum_destination': {
+                    'sum': {
+                        'field': dstField
+                        }
+                    }
+                }
+        result = es.search(index=index, query=query, aggs=aggregation_query)
+        totals[value]['source']=result['aggregations']['total_sum_source']['value']
+        totals[value]['destination']=result['aggregations']['total_sum_destination']['value']
+    return totals
+
+def get_documents_counts(field: str) -> dict:
+    '''
+    Returns the documents counts sorted with field values
+    '''
+    values = get_values(field)
+    counts = {}
+    for value in values:
+        counts[value] = get_documents_count(field,value)
+    return counts
+
+#############
+# PROTOCOLS #
+#############
+
+def get_protocols() -> list:
+    """
+    Returns the list of all the (distinct) protocols contained in the XML files
+    """
+    return get_values('protocolName')
+
 def get_protocol_flows(protocol: str) -> list:
     '''
     Returns the list of flows for a given protocol
     '''
-    return get_term_flows('protocolName',protocol) 
+    return get_documents('protocolName',protocol) 
 
 def get_protocols_flows_count() -> dict:
     '''
     Returns the number of flows for each protocols
     '''
-    protocols = get_protocols()
-    counts = {}
-    for protocol in protocols:
-        counts[protocol] = 0
-        search_query = {
-                'match': {
-                    'protocolName': protocol
-                    }
-                }
-        result = es.count(index=index, query=search_query)
-        counts[protocol] += result['count']
-    return counts
+    return get_documents_counts('protocolName')
 
 def get_protocols_total_bytes() -> dict:
     '''
-    Returns  the total source/destination Bytes for each protocol
+    Returns the total source/destination Bytes for each protocol
     '''
-    protocols = get_protocols()
-    total_bytes = {}
-    for protocol in protocols:
-        total_bytes[protocol] = {'source': 0, 'destination': 0}
-        query = {
-                'term': {
-                    'protocolName': protocol
-                    }
-                }
-        aggregation_query = {
-                'total_sum_source': {
-                    'sum': {
-                        'field': 'totalSourceBytes'
-                        }
-                    },
-                'total_sum_destination': {
-                    'sum': {
-                        'field': 'totalDestinationBytes'
-                        }
-                    }
-                }
-        result = es.search(index=index, query=query, aggs=aggregation_query)
-        total_bytes[protocol]['source'] += result['aggregations']['total_sum_source']['value']
-        total_bytes[protocol]['destination'] += result['aggregations']['total_sum_destination']['value']
-    return total_bytes
-
+    return get_totals('protocolName','totalSourceBytes','totalDestinationBytes')
 
 def get_protocols_total_packets() -> dict:
     '''
-    Returns  the total source/destination Bytes for each protocol
+    Returns the total source/destination Bytes for each protocol
     '''
-    protocols = get_protocols()
-    total_bytes = {}
-    for protocol in protocols:
-        total_packets[protocol] = {'source': 0, 'destination': 0}
-        query = {
-                'term': {
-                    'protocolName': protocol
-                    }
-                }
-        aggregation_query = {
-                'total_sum_source': {
-                    'sum': {
-                        'field': 'totalSourceBytes'
-                        }
-                    },
-                'total_sum_destination': {
-                    'sum': {
-                        'field': 'totalDestinationBytes'
-                        }
-                    }
-                }
-        result = es.search(index=index, query=query, aggs=aggregation_query)
-        total_packets[protocol]['source'] += result['aggregations']['total_sum_source']['value']
-        total_packets[protocol]['destination'] += result['aggregations']['total_sum_destination']['value']
-    return total_packets
+    return get_totals('protocolName','totalSourcePackets','totalDestinationPackets')
+
+################
+# APPLICATIONS #
+################
 
 def get_apps() -> list:
     """
     Returns the list of all the (distinct) apps contained in the XML files
     """
-    apps = []
-    aggregation_query = {'unique_values':{'terms':{'field':'appName','size':1000000}}}
-    result = es.search(index=index, aggs=aggregation_query)
-    apps += [bucket["key"] for bucket in result["aggregations"]["unique_values"]["buckets"]]
-    return apps
-
-def get_app_flows_count(app: str) -> int:
-    '''
-    Returns the number of flows for a single app
-    '''
-    search_query = {'match': {'appName': app}}
-    return es.count(index=index, query=search_query)['count']
+    return get_values('appName')
 
 def get_app_flows(app: str) -> list:
     '''
     Returns the list of flows for a given app
     '''
-    return get_term_flows('appName',app)
+    return get_documents('appName',app)
 
 def get_apps_flows_count() -> dict:
     '''
     Returns the number of flows for each apps
     '''
-    apps = get_apps()
-    counts = {}
-    for app in apps:
-        counts[app] = 0
-        search_query = {'match': {'appName': app}}
-        result = es.count(index=index, query=search_query)
-        counts[app] += result['count']
-    return counts
+    return get_documents_counts('appName')
 
 def get_apps_total_bytes() -> dict:
     '''
-    Returns  the total source/destination Bytes for each app
+    Returns the total source/destination Bytes for each app
     '''
-    apps = get_apps()
-    total_bytes = {}
-    for app in apps:
-        total_bytes[app] = {'source': 0, 'destination': 0}
-        query = {
-            'term': {
-                'appName': app
-            }
-        }
-        aggregation_query = {
-            'total_sum_source': {
-                'sum': {
-                    'field': 'totalSourceBytes'
-                }
-            },
-            'total_sum_destination': {
-                'sum': {
-                    'field': 'totalDestinationBytes'
-                }
-            }
-        }
-        result = es.search(index=index, query=query, aggs=aggregation_query)
-        total_bytes[app]['source'] += result['aggregations']['total_sum_source']['value']
-        total_bytes[app]['destination'] += result['aggregations']['total_sum_destination']['value']
-    return total_bytes
-
+    return get_totals('appName','totalSourceBytes','totalDestinationBytes')
 
 def get_apps_total_packets() -> dict:
     '''
-    Returns  the total source/destination Bytes for each app
+    Returns the total source/destination packets for each app
     '''
-    apps = get_apps()
-    total_bytes = {}
-    for app in apps:
-        total_packets[app] = {'source': 0, 'destination': 0}
-        query = {
-            'term': {
-                'appName': app
-            }
-        }
-        aggregation_query = {
-            'total_sum_source': {
-                'sum': {
-                    'field': 'totalSourceBytes'
-                }
-            },
-            'total_sum_destination': {
-                'sum': {
-                    'field': 'totalDestinationBytes'
-                }
-            }
-        }
-        result = es.search(index=index, query=query, aggs=aggregation_query)
-        total_packets[app]['source'] += result['aggregations']['total_sum_source']['value']
-        total_packets[app]['destination'] += result['aggregations']['total_sum_destination']['value']
-    return total_packets
+    return get_totals('appName','totalSourcePackets','totalDestinationPackets')
 
 
--- scripts/api_demo.py
+++ scripts/api_demo.py
@@ -3,15 +3,6 @@ import api
 def print_green(skk): print("\033[92m {}\033[00m" .format(skk))
 
 print('Testing API')
-print('indices: ', api.get_indices())
-protocols=api.get_protocols()
-print('protocols: ',protocols) 
-#print('flows with the protocol ', protocols[0], ': ', api.get_flows(protocols[0]))
-print_green('get_protocols_flows_count()')
-print(api.get_protocols_flows_count())
-print_green('get_protocols_total_bytes()')
-print(api.get_protocols_total_bytes())
-
 print_green('apps: ')
 print(api.get_apps()) 
 #print('flows with the app ', apps[0], ': ', api.get_flows(apps[0]))
